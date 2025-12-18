850383e65cb96422f8d3a3a6c62a2172b803f394
milicaradicc <milica.t.radic@gmail.com>
2025-12-18 09:36:42 +0100

    Update config

--- simulation/simulators/dht.py
+++ simulation/simulators/dht.py
@@ -15,10 +15,9 @@ def generate_values(initial_temp = 25, initial_humidity=20):
 
       
 
-def run_dht_simulator(delay, callback, stop_event):
-        for h, t in generate_values():
-            time.sleep(delay)  # Delay between readings (adjust as needed)
-            callback(h, t)
-            if stop_event.is_set():
-                  break
-              
+def run_dht_simulator(delay, callback, stop_event, code="DHT1"):
+    for h, t in generate_values():
+        time.sleep(delay)
+        callback(h, t, code)
+        if stop_event.is_set():
+            break
