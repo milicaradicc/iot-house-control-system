ed7df8e0ae11a560a42f7b8da1c950987ca374a4
milicaradicc <milica.t.radic@gmail.com>
2025-12-22 10:18:31 +0100

    [MOD] dpir

--- simulation/components/dpir1.py
+++ simulation/components/dpir1.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import random
+from ..simulators.dpir1 import run_motion_sensor_simulator
 
 def dpir1_callback(motion_detected, code="DPIR1"):
     t = time.localtime()
@@ -8,23 +9,23 @@ def dpir1_callback(motion_detected, code="DPIR1"):
     print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
     print(f"Code: {code}")
     print(f"Motion detected: {'YES' if motion_detected else 'NO'}")
+    print("="*20)
 
 def generate_motion_values():
     while True:
         yield random.choice([True, False])
 
 def run_motion_sensor(settings, threads, stop_event):
+    code = "DPIR1"
+
     if settings.get('simulated', True):
         print("Starting DPIR1 simulator")
 
-        def sensor_loop():
-            for state in generate_motion_values():
-                if stop_event.is_set():
-                    break
-                dpir1_callback(state, code="DPIR1")
-                time.sleep(settings.get('delay', 2)) 
+        t = threading.Thread(
+            target=run_motion_sensor_simulator,
+            args=(dpir1_callback, stop_event, code)
+        )
 
-        t = threading.Thread(target=sensor_loop)
         t.start()
         threads.append(t)
 
@@ -32,8 +33,15 @@ def run_motion_sensor(settings, threads, stop_event):
 
     else:
         from sensors.dpir1 import DPIR1Sensor, run_dpir1_loop
+
         sensor = DPIR1Sensor(settings['pin'])
-        t = threading.Thread(target=run_dpir1_loop, args=(sensor, settings.get('delay', 2), dpir1_callback, stop_event))
+
+        t = threading.Thread(
+            target=run_dpir1_loop,
+            args=(sensor, settings.get('delay', 2), dpir1_callback, stop_event)
+        )
+
         t.start()
         threads.append(t)
+
         print("DPIR1 hardware loop started")
